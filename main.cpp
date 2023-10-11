26c56d667a507bd19fbac5ee2c2d7afc415ef712
ruka <115062996+merutao@users.noreply.github.com>
2023-10-11 14:14:20 +0900

    さんっっじょう！！！

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include "TextureManager.h"
 #include "WinApp.h"
 
+//さんっっじょう！！！！！！！！！
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	WinApp* win = nullptr;
